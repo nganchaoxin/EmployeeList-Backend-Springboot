99447944108150adc6d166b9f31ee54439154b06
nganchaoxin <kimnganphamsu01@gmail.com>
2023-05-14 09:44:03 +0700

    first commit

--- src/main/java/com/example/demo/TaskManagementApplication.java
+++ src/main/java/com/example/demo/TaskManagementApplication.java
@@ -23,26 +23,26 @@ public class TaskManagementApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		EmployeeEntity employee1 = EmployeeEntity.builder()
-				.firstName("Ramesh")
-				.lastName("Fadatare")
-				.email("ramesh@gmail.com")
-				.build();
-
-		EmployeeEntity employee2 = EmployeeEntity.builder()
-				.firstName("Tony")
-				.lastName("Stark")
-				.email("tony@gmail.com")
-				.build();
-
-		EmployeeEntity employee3 = EmployeeEntity.builder()
-				.firstName("John")
-				.lastName("Cena")
-				.email("cena@gmail.com")
-				.build();
-
-		employeeRepository.save(employee1);
-		employeeRepository.save(employee2);
-		employeeRepository.save(employee3);
+//		EmployeeEntity employee1 = EmployeeEntity.builder()
+//				.firstName("Ramesh")
+//				.lastName("Fadatare")
+//				.email("ramesh@gmail.com")
+//				.build();
+//
+//		EmployeeEntity employee2 = EmployeeEntity.builder()
+//				.firstName("Tony")
+//				.lastName("Stark")
+//				.email("tony@gmail.com")
+//				.build();
+//
+//		EmployeeEntity employee3 = EmployeeEntity.builder()
+//				.firstName("John")
+//				.lastName("Cena")
+//				.email("cena@gmail.com")
+//				.build();
+//
+//		employeeRepository.save(employee1);
+//		employeeRepository.save(employee2);
+//		employeeRepository.save(employee3);
 	}
 }
--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin("http://localhost:3000/")
+@CrossOrigin("http://localhost:8082")
 public class EmployeeController {
     @Autowired
     EmployeeRepository employeeRepository;
